a64b83f9d06c9fba73707bafddbde090a0db2034
Juan Koch <juanandreskochp@gamil.com>
2025-08-28 11:24:46 -0400

    Genero dtos

---
+++ DTOS/Generos/CrearGeneroDto.cs
@@ -0,0 +1,7 @@
+namespace TestApi.DTOS
+{
+    public class CrearGeneroDto
+    {
+        public string Genero1 { get; set; } = null!;
+    }
+}
---
+++ DTOS/Generos/GeneroDto.cs
@@ -0,0 +1,9 @@
+namespace TestApi.DTOS
+{
+    public class GeneroDto
+    {
+        public int Id { get; set; }
+
+        public string Genero1 { get; set; } = null!;
+    }
+}
